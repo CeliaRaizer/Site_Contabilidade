654eabc99b2912db02277df2944267b49572b5e7
Célia Cristina Raizer <celia.cristina.slongo.raizer@gmail.com>
2026-02-12 21:59:57 -0300

    imagem

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,31 @@
+import heroImage from "../assets/hero.jpg";
+import "./Hero.css";
+
+
 function Hero() {
   return (
-    <section>
-     
+    <section
+      className="hero"
+      style={{ backgroundImage: `url(${heroImage})` }}
+    >
+      <div className="overlay"></div>
+
+      <div className="hero-content">
+        <h1>
+          Contabilidade Descomplicada para quem quer crescer de verdade
+          <br />
+        </h1>
+
+        <p>
+          Cuidamos do seu MEI ou empresa do Simples enquanto você foca no 
+          que realmente importa: faturar.        
+        </p>
+
+        <div className="buttons">
+          <button className="primary">Abrir Empresa</button>
+          <button className="secondary">Trocar de Contador</button>
+        </div>
+      </div>
     </section>
   );
 }
