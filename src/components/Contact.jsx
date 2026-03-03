61a3daa763a9510f1673ab7de51fe022bef1a075
Célia Cristina Raizer <celia.cristina.slongo.raizer@gmail.com>
2026-03-03 07:47:43 -0300

    botão insta

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,29 +1,31 @@
+import { 
+  FaInstagram, 
+  FaFacebookF, 
+  FaLinkedinIn, 
+  FaXTwitter, 
+  FaWhatsapp 
+} from "react-icons/fa6";
+
+import "./Contact.css";
+
 function Contact() {
   return (
-    <section className="contact" id="contato">
-      <h2>Contato</h2>
-      <p>
-        📞 WhatsApp: 
-        <a 
-          href="https://wa.me/5549998249058" 
-          target="_blank" 
-          rel="noopener noreferrer"
-        >
-          (49) 99824-9058
-        </a>
-      </p>
-      <p>
-        📸 Instagram: 
-        <a 
-          href="https://www.instagram.com/contadora.elainefae" 
-          target="_blank" 
-          rel="noopener noreferrer"
-        >
-           @contadora.elainefae
-        </a>
-      </p>
-      <p>📍 Atendimento online e personalizado</p>
-    </section>
+    <div className="floating-icons">
+      
+      <a href="#" className="icon-btn">
+        <FaInstagram />
+      </a>
+
+      <a 
+        href="https://wa.me/5549998249058"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="icon-btn"
+      >
+        <FaWhatsapp />
+      </a>
+
+    </div>
   );
 }
 
